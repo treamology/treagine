afc814e1602f3640c3b4cbe163a1e4273e0ceea4
Donny Lawrence <trea@treamous.com>
2016-06-20 11:35:03 -0500

    Update getCenter and setCenter to use either scale or size.

--- game/entity.lua
+++ game/entity.lua
@@ -11,12 +11,31 @@ function Entity:init()
 end
 
 function Entity:getCenter()
-	return self.position + (self.size / 2)
+	if self.image then
+		local sizeX = self.image:getWidth() * self.scale.x
+		local sizeY = self.image:getHeight() * self.scale.y
+		return self.position + (vector(sizeX, sizeY) / 2)
+	elseif self.size then
+		return self.position + (self.size / 2)
+	end
+	return self.position
 end
 
 function Entity:setCenter(x, y)
-	self.position.x = x - (self.size.x / 2)
-	self.position.y = y - (self.size.y / 2)
+	if self.image then
+		local sizeX = self.image:getWidth() * self.scale.x
+		local sizeY = self.image:getHeight() * self.scale.y
+		self.position.x = x - (sizeX / 2)
+		self.position.y = y - (sizeY / 2)
+		return
+	elseif self.size then
+		self.position.x = x - (self.size.x / 2)
+		self.position.y = y - (self.size.y / 2)
+		return
+	end
+	
+	self.position.x = x
+	self.position.y = y
 end
 
 return Entity
