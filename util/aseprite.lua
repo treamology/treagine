7469136fd36f3c200827faeb0e0d9a745f1ec9cd
Donny Lawrence <trea@treamous.com>
2016-06-22 14:04:57 -0500

    Be sure animation frames are sorted properly when loaded.

--- util/aseprite.lua
+++ util/aseprite.lua
@@ -5,13 +5,23 @@ local Aseprite = {}
 
 
 function Aseprite.importAnimation(name)
-
+	-- load up the json and decode it so it can be read like a table
 	local rawJSON = love.filesystem.read("assets/sprites/data/" .. name .. ".json")
 	local animData = json:decode(rawJSON)
 
-	local frames = {}
+	-- seperate the frames from their keys and sort them by position in the image.
+	-- this needs to be done because by default the frames are in an arbitrary order in animData
+	local sortedFrames = {}
+	for _, v in pairs(animData.frames) do
+		table.insert(sortedFrames, v)
+	end
+	table.sort(sortedFrames, function(a, b)
+		return a.frame.x < b.frame.x
+	end)
 
-	for _, frame in pairs(animData.frames) do
+	-- extract needed data from the sorted frames
+	local frames = {}
+	for _, frame in pairs(sortedFrames) do
 		local frameInfo = frame.frame
 
 		local out = {}
@@ -32,18 +42,18 @@ function Aseprite.importAnimation(name)
 	local function createDurations(from, to)
 		local durations = {}
 		for i = from, to do
-			table.insert(durations, frames[i].duration)	
+			table.insert(durations, frames[i].duration / 1000)
 		end
 		return durations
 	end
 
+	-- finally, create the anim8 animations from the frame data
 	local animTable = {}
-
 	for _, tag in pairs(animData.meta.frameTags) do
 		animTable[tag.name] = anim8.newAnimation(grid(tag.from + 1 .. "-" .. tag.to + 1, 1), createDurations(tag.from + 1, tag.to + 1))
 	end
 
-	return animTable
+	return image, animTable
 end
 
 return Aseprite
