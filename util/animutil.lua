9a01119805a0e8395e5b8f1daf0184efd8955cb0
Donny Lawrence <trea@treamous.com>
2016-06-25 14:51:00 -0500

    Add ability to traverse both rows and columns of a spritesheet generated from Aseprite. Animation importer can also cache sorted frames now.

--- util/animutil.lua
+++ util/animutil.lua
@@ -6,59 +6,72 @@ local animutil = {}
 local animDataCache = {}
 
 function animutil.importAnimations(name)
-	local animData
+	local animData, fromCache
 	if animDataCache[name] then
 		-- just load what's in the cache
 		animData = animDataCache[name]
+		fromCache = true
 	else
 		-- load up the json and decode it, then cache it
 		local rawJSON = love.filesystem.read("assets/sprites/data/" .. name .. ".json")
 		animData = json:decode(rawJSON)
 		animDataCache[name] = animData
+		fromCache = false
 	end
-
-	-- separate the frames from their keys and sort them by position in the image.
-	-- this needs to be done because by default the frames are in an arbitrary order in animData
-	local sortedFrames = {}
-	for _, v in pairs(animData.frames) do
-		table.insert(sortedFrames, v)
-	end
-	table.sort(sortedFrames, function(a, b)
-		return a.frame.x < b.frame.x
-	end)
 
 	-- extract needed data from the sorted frames
-	local frames = {}
-	for _, frame in pairs(sortedFrames) do
-		local frameInfo = frame.frame
+	if not fromCache then
+		local frameData = {}
+		for _, frame in pairs(animData.frames) do
+			local frameInfo = frame.frame
 
-		local out = {}
-		out.x = frameInfo.x
-		out.y = frameInfo.y
-		out.w = frameInfo.w
-		out.h = frameInfo.h
-		out.duration = frame.duration
+			local out = {}
+			out.x = frameInfo.x
+			out.y = frameInfo.y
+			out.w = frameInfo.w
+			out.h = frameInfo.h
+			out.duration = frame.duration
 
-		table.insert(frames, out)
+			out.xLoc = out.x / out.w
+			out.yLoc = out.y / out.h
+
+			table.insert(frameData, out)
+		end
+
+		-- sort the frames based on their position in the image.
+		table.sort(frameData, function(a, b)
+			return a.y < b.y or (a.y == b.y and a.x < b.x)
+		end)
+
+		animData.frameData = frameData
 	end
 
 	-- for now, all the frames have to be the same size
 	-- support for multiple grids can be added later.
 	local image = love.graphics.newImage("assets/sprites/" .. name .. ".png")
-	local grid = anim8.newGrid(frames[1].w, frames[2].h, image:getDimensions())
+	local grid = anim8.newGrid(animData.frameData[1].w, animData.frameData[2].h, image:getDimensions())
 
 	local function createDurations(from, to)
 		local durations = {}
 		for i = from, to do
-			table.insert(durations, frames[i].duration / 1000)
+			table.insert(durations, animData.frameData[i].duration / 1000)
 		end
 		return durations
+	end
+	local function getCoordinates(from, to)
+		local coords = {}
+		for i = from, to do
+			table.insert(coords, animData.frameData[i].xLoc + 1)
+			table.insert(coords, animData.frameData[i].yLoc + 1)
+		end
+		return unpack(coords)
 	end
 
 	-- finally, create the anim8 animations from the frame data
 	local animTable = {}
 	for _, tag in pairs(animData.meta.frameTags) do
-		animTable[tag.name] = anim8.newAnimation(grid(tag.from + 1 .. "-" .. tag.to + 1, 1), createDurations(tag.from + 1, tag.to + 1))
+		local xStart = animData.frameData[tag.from]
+		animTable[tag.name] = anim8.newAnimation(grid(getCoordinates(tag.from + 1, tag.to + 1)), createDurations(tag.from + 1, tag.to + 1))
 	end
 
 	return image, animTable
