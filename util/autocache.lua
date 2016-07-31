4af7ab04d8337bff829abfcae36a8c70bc77b791
Donny Lawrence <trea@treamous.com>
2016-07-31 13:05:08 -0500

    Add module that overrides love's image and font loading, loads from cache before loading from disk.

---
+++ util/autocache.lua
@@ -0,0 +1,48 @@
+local autocache = {}
+
+local new_image = love.graphics.newImage
+local new_font = love.graphics.newFont
+local new_image_font = love.graphics.newImageFont
+
+local cachedImages = {}
+local cachedFonts = {}
+
+local function newImage(name, ...)
+	if type(name) ~= "string" then
+		return new_image(name, ...)
+	end
+
+	if cachedImages[name] == nil then
+		print("Loading image " .. name)
+		cachedImages[name] = new_image(name, ...)
+	end
+
+	return cachedImages[name]
+end
+
+local function newFont(name, ...)
+	if cachedFonts[name] == nil then
+		print("Loading font " .. name)
+		cachedFonts[name] = new_font(name, ...)
+	end
+
+	return cachedFonts[name]
+end
+
+local function newImageFont(...)
+	if cachedFonts[name] == nil then
+		print("Loading font " .. name)
+		cachedFonts[name] = new_image_font(name, ...)
+	end
+
+	return cachedFonts[name]
+end
+
+love.graphics.newImage = newImage
+love.graphics.newFont = newFont
+love.graphics.newImageFont = newImageFont
+
+autocache.cachedImages = cachedImages
+autocache.cachedFonts = cachedFonts
+
+return autocache
