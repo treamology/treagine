5bbf12b0bb27e047ff81f36fe23b41420594e4f8
Donny Lawrence <trea@treamous.com>
2016-09-03 22:59:48 -0500

    Add point drawing.

--- system/debugdrawsystem.lua
+++ system/debugdrawsystem.lua
@@ -8,6 +8,7 @@ local DebugDrawSystem = class("DebugDrawSystem")
 function DebugDrawSystem:init(screen)
 	self.rectList = {}
 	self.lineList = {}
+	self.pointList = {}
 	self.drawFPS = false
 	self.showEntityBounds = false
 	self.showEntityAnchors = false
@@ -16,6 +17,7 @@ function DebugDrawSystem:init(screen)
 
 	beholder.observe("debug", "drawRectangle", function(table) self:addRectangle(table) end)
 	beholder.observe("debug", "drawLine", function(table) self:addLine(table) end)
+	beholder.observe("debug", "drawPoint", function(table) self:addPoint(table) end)
 	beholder.observe("debug", "showEntityBounds", function(bool) self.showEntityBounds = bool end)
 	beholder.observe("debug", "showEntityAnchors", function(bool) self.showEntityAnchors = bool end)
 end
@@ -94,12 +96,33 @@ function DebugDrawSystem:draw()
 
 	for k, v in pairs(self.lineList) do
 		local color = v.color or {255, 0, 0, 255}
+		local screenSpace = v.screenSpace or false
 
-		local px1, py1 = self.screen.viewport:projectLight(v.x1, v.y1)
-		local px2, py2 = self.screen.viewport:projectLight(v.x2, v.y2)
+		local px1, py1, px2, py2
+		if screenSpace then
+			px1, py1, px2, py2 = v.x1, v.y1, v.x2, v.y2
+		else
+			px1, py1 = self.screen.viewport:projectLight(v.x1, v.y1)
+			px2, py2 = self.screen.viewport:projectLight(v.x2, v.y2)
+		end
 
 		love.graphics.setColor(color)
 		love.graphics.line(px1, py1, px2, py2)
+	end
+
+	for k, v in pairs(self.pointList) do
+		local color = v.color or {255, 0, 0, 255}
+		local screenSpace = v.screenSpace or false
+
+		local px, py
+		if screenSpace then
+			px, py = v.x, v.y
+		else
+			px, py = self.screen.viewport:projectLight(v.x, v.y)
+		end
+
+		love.graphics.setColor(color)
+		love.graphics.points(px, py)
 	end
 
 	for k, v in pairs(self.rectList) do
@@ -107,6 +130,9 @@ function DebugDrawSystem:draw()
 	end
 	for k, v in pairs(self.lineList) do
 		self.lineList[k] = nil
+	end
+	for k, v in pairs(self.pointList) do
+		self.pointList[k] = nil
 	end
 end
 
@@ -116,6 +142,10 @@ end
 
 function DebugDrawSystem:addLine(line)
 	table.insert(self.lineList, line)
+end
+
+function DebugDrawSystem:addPoint(point)
+	table.insert(self.pointList, point)
 end
 
 return DebugDrawSystem
